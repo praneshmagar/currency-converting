feb857f6ee7311454c454c2db22e9069732b9831
pranesh magar <praneshmagar05@gmail.com>
2026-08-18 12:32:50 -0400

    Add initial currency converter

---
+++ converter-project.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//1. get currecy the person has 
+//2. ask what currecny they want converted to
+//3. function takes that currency and does the math required we will be doing usd to euro and yen for now because idk 
+// apis and grabbing things from internet
+//4. print out the conversion using function and organize functions to be in specfic areas
+// 
+
+// ask the user for to choose between 3 currencies for what they have then again for what they want converted
+// user input two currency
+
+//functions needed - conversion that takes in the choices and if they something like 0 for usd 2 for yen put in those 
+// and do the math for that
+// then return the conversion
+// print conversion
+
+
+
+int main()
+{
+    
+    
+
+
+    return 0;
+}
